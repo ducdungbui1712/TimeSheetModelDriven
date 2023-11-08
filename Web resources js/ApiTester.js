ebcfe6af76ec63f62be998a0562d4a4cccfd63eb
NTangHao <haohan2801@gmail.com>
2023-11-08 11:29:21 +0700

    update

--- Web resources js/ApiTester.js
+++ Web resources js/ApiTester.js
@@ -1,6 +1,3 @@
-// Online Javascript Editor for free
-// Write, Edit and Run your Javascript code using JS Online Compiler
-
 var timesheet = [
   {
     createdby: "Hao",
@@ -8,32 +5,17 @@ var timesheet = [
     new_date: "2023-10-17",
     new_currentstatus: "Office",
   },
-
-  {
-    createdby: "Dung",
-    new_duration: 8,
-    new_date: "2023-10-11",
-    new_currentstatus: "Office",
-  },
-
-  {
-    createdby: "Dung",
-    new_duration: 3,
-    new_date: "2023-10-11",
-    new_currentstatus: "After22h",
-  },
-  {
-    createdby: "Dung",
-    new_duration: 8,
-    new_date: "2023-10-12",
-    new_currentstatus: "Office",
-  },
-
-  {
-    createdby: "Dung",
+   {
+    createdby: "Hao",
     new_duration: 2,
-    new_date: "2023-10-12",
+    new_date: "2023-10-17",
     new_currentstatus: "After22h",
+  },
+  {
+    createdby: "Dung",
+    new_duration: 8,
+    new_date: "2023-10-11",
+    new_currentstatus: "Before22h",
   },
   {
     createdby: "Hao",
@@ -42,10 +24,16 @@ var timesheet = [
     new_currentstatus: "Office",
   },
   {
+    createdby: "Hao",
+    new_duration: 7,
+    new_date: "2023-10-17",
+    new_currentstatus: "Before22h",
+  },
+  {
     createdby: "Khang",
     new_duration: 8,
     new_date: "2023-08-11",
-    new_currentstatus: "After22h",
+    new_currentstatus: "Before22h",
   },
 ];
 
@@ -67,15 +55,50 @@ var vendoremployee = [
 var vendor = [
   {
     vendorid: "192134141df23",
-    OTrate: 1,
-    after22h: 3,
+    newweekdayoffice: 1,
+    newweekdaybefore22h: 1.5,
+    newweekdayafter22h: 1.5,
+    newweekendbefore22h:2,
+    newweekendafter22h:2,
+ 
   },
   {
     vendorid: "113df",
-    OTrate: 1,
-    after22h: 3,
+    newweekdayoffice: 1,
+    newweekdaybefore22h: 1.5,
+    newweekdayafter22h: 1.5,
+    newweekendbefore22h:2,
+    newweekendafter22h:2,
+  
   },
 ];
+
+
+
+var result = timesheet.map((ts) => {
+  var ve = vendoremployee.find((ve) => ve.employee_name === ts.createdby);
+  var v = vendor.find((v) => v.vendorid === ve.vendoridRef);
+  return { ...ts, ...ve, ...v };
+});
+
+console.log(result)
+
+function OtHour(item){
+	let value = 1;
+	let date = new Date(item.new_date);
+	let day = date.getDay();
+	if(day >= 1 && day <= 5 && item.new_currentstatus == "Office")
+		value = item.new_duration * item.newweekdayoffice;
+	else if (day >= 1 && day <= 5 && item.new_currentstatus == "Before22h")
+		value = item.new_duration * item.newweekdaybefore22h;
+	else if (day >= 1 && day <= 5 && item.new_currentstatus == "After22h")
+		value = item.new_duration * item.newweekdayafter22h;
+	else if ((day === 0 || day === 6) && item.new_currentstatus == "Before22h")
+		value = item.new_duration * item.newweekendbefore22h;
+	else if ((day === 0 || day === 6) && item.new_currentstatus == "After22h")
+		value = item.new_duration * item.newweekendafter22h;
+	return value;
+}
 
 function groupByDayAndCreatedBy(rawData, month, year) {
   var daysInMonth = new Date(year, month, 0).getDate();
@@ -111,50 +134,57 @@ function groupByDayAndCreatedBy(rawData, month, year) {
       var status = item.new_currentstatus;
       if(status == "Office"){
         if (temp[item.createdby][totalOfficeHours_index]) {
-        temp[item.createdby][totalOfficeHours_index] += item.new_duration;
+        temp[item.createdby][totalOfficeHours_index] += OtHour(item);
         } else {
-        temp[item.createdby][totalOfficeHours_index] = item.new_duration;
+        temp[item.createdby][totalOfficeHours_index] = OtHour(item);
         }
       }
       if(status == "After22h"){
         if (temp[item.createdby][totalAfter22h_index]) {
-        temp[item.createdby][totalAfter22h_index] += item.new_duration;
+        temp[item.createdby][totalAfter22h_index] += OtHour(item);
         } else {
-        temp[item.createdby][totalAfter22h_index] = item.new_duration;
+        temp[item.createdby][totalAfter22h_index] = OtHour(item);
         }
       }
       if(status == "Before22h"){
         if (temp[item.createdby][totalBefore22h_index]) {
-        temp[item.createdby][totalBefore22h_index] += item.new_duration;
+        temp[item.createdby][totalBefore22h_index] += OtHour(item);
         } else {
-        temp[item.createdby][totalBefore22h_index] = item.new_duration;
+        temp[item.createdby][totalBefore22h_index] = OtHour(item);
         }
       }
     }
   });
 
-  console.log(temp)
+  //console.log(temp)
 
   // Chuyển dữ liệu từ object tạm sang mảng kết quả
   for (var key in temp) {
-    var obj = { createdby: key };
+    var obj = { Employee: key };
     for (var i = 1; i <= daysInMonth; i++) {
       obj[i] = temp[key][i];
     }
     obj["TotalOfficeHours"] = temp[key][totalOfficeHours_index];
     obj["TotalBefore22h"] = temp[key][totalBefore22h_index];
     obj["TotalAfter22h"] = temp[key][totalAfter22h_index];
+    obj["TotalWorkingHours"] = obj["TotalOfficeHours"] + obj["TotalBefore22h"] + obj["TotalAfter22h"]
     result.push(obj);
   }
   return result;
 }
 
-var result = timesheet.map((ts) => {
-  var ve = vendoremployee.find((ve) => ve.employee_name === ts.createdby);
-  var v = vendor.find((v) => v.vendorid === ve.vendoridRef);
-  return { ...ts, ...ve, ...v };
-});
+console.log(groupByDayAndCreatedBy(result,10,2023))
 
-// console.log(timesheet);
 
-console.log(groupByDayAndCreatedBy(timesheet, 10, 2023));
+
+
+
+
+
+
+
+
+
+
+
+
